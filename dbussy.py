f62883e9bddfebd56315a25f665dfb49d514d280
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2017-04-20 08:47:16 +0000

    Successfully got a connection, requested a bus name and released it

--- dbussy.py
+++ dbussy.py
@@ -5,8 +5,9 @@
 # libdbus API: <https://dbus.freedesktop.org/doc/api/html/index.html>.
 #-
 
-import enum
 import ctypes as ct
+from weakref import \
+    WeakValueDictionary
 import atexit
 
 dbus = ct.cdll.LoadLibrary("libdbus-1.so.3")
@@ -21,6 +22,230 @@ class DBUS :
     # the refcount goes to 0 before the routine is actually entered, and the object
     # can get prematurely disposed. Always store the object reference into a local
     # variable, and pass the value of the variable instead.
+
+    # from dbus-protocol.h:
+
+    # Message byte order
+    LITTLE_ENDIAN = 'l'
+    BIG_ENDIAN = 'B'
+
+    # Protocol version.
+    MAJOR_PROTOCOL_VERSION = 1
+
+    # Type code that is never equal to a legitimate type code
+    TYPE_INVALID = 0
+
+    # Primitive types
+    TYPE_BYTE = ord('y') # 8-bit unsigned integer
+    TYPE_BOOLEAN = ord('b') # boolean
+    TYPE_INT16 = ord('n') # 16-bit signed integer
+    TYPE_UINT16 = ord('q') # 16-bit unsigned integer
+    TYPE_INT32 = ord('i') # 32-bit signed integer
+    TYPE_UINT32 = ord('u') # 32-bit unsigned integer
+    TYPE_INT64 = ord('x') # 64-bit signed integer
+    TYPE_UINT64 = ord('t') # 64-bit unsigned integer
+    TYPE_DOUBLE = ord('d') # 8-byte double in IEEE 754 format
+    TYPE_STRING = ord('s') # UTF-8 encoded, nul-terminated Unicode string
+    TYPE_OBJECT_PATH = ord('o') # D-Bus object path
+    TYPE_SIGNATURE = ord('g') # D-Bus type signature
+    TYPE_UNIX_FD = ord('h') # unix file descriptor
+
+    # Compound types
+    TYPE_ARRAY = ord('a') # D-Bus array type
+    TYPE_VARIANT = ord('v') # D-Bus variant type
+
+    TYPE_STRUCT = ord('r') # a struct; however, type signatures use STRUCT_BEGIN/END_CHAR
+    TYPE_DICT_ENTRY = ord('e') # a dict entry; however, type signatures use DICT_ENTRY_BEGIN/END_CHAR
+    NUMBER_OF_TYPES = 16 # does not include TYPE_INVALID or STRUCT/DICT_ENTRY_BEGIN/END_CHAR
+
+    # characters other than typecodes that appear in type signatures
+
+    STRUCT_BEGIN_CHAR = ord('(') # start of a struct type in a type signature
+    STRUCT_END_CHAR = ord(')') # end of a struct type in a type signature
+    DICT_ENTRY_BEGIN_CHAR = ord('{') # start of a dict entry type in a type signature
+    DICT_ENTRY_END_CHAR = ord('}') # end of a dict entry type in a type signature
+
+    MAXIMUM_NAME_LENGTH = 255 # max length in bytes of a bus name, interface or member (object paths are unlimited)
+
+    MAXIMUM_SIGNATURE_LENGTH = 255 # fits in a byte
+
+    MAXIMUM_MATCH_RULE_LENGTH = 1024
+
+    MAXIMUM_MATCH_RULE_ARG_NUMBER = 63
+
+    MAXIMUM_ARRAY_LENGTH = 67108864 # 2 * 26
+    MAXIMUM_ARRAY_LENGTH_BITS = 26 # to store the max array size
+
+    MAXIMUM_MESSAGE_LENGTH = MAXIMUM_ARRAY_LENGTH * 2
+    MAXIMUM_MESSAGE_LENGTH_BITS = 27
+
+    MAXIMUM_MESSAGE_UNIX_FDS = MAXIMUM_MESSAGE_LENGTH // 4 # FDs are at least 32 bits
+    MAXIMUM_MESSAGE_UNIX_FDS_BITS = MAXIMUM_MESSAGE_LENGTH_BITS - 2
+
+    MAXIMUM_TYPE_RECURSION_DEPTH = 32
+
+    # Types of message
+
+    MESSAGE_TYPE_INVALID = 0 # never a valid message type
+    MESSAGE_TYPE_METHOD_CALL = 1
+    MESSAGE_TYPE_METHOD_RETURN = 2
+    MESSAGE_TYPE_ERROR = 3
+    MESSAGE_TYPE_SIGNAL = 4
+
+    NUM_MESSAGE_TYPES = 5
+
+    # Header flags
+
+    HEADER_FLAG_NO_REPLY_EXPECTED = 0x1
+    HEADER_FLAG_NO_AUTO_START = 0x2
+    HEADER_FLAG_ALLOW_INTERACTIVE_AUTHORIZATION = 0x4
+
+    # Header fields
+
+    HEADER_FIELD_INVALID = 0
+    HEADER_FIELD_PATH = 1
+    HEADER_FIELD_INTERFACE = 2
+    HEADER_FIELD_MEMBER = 3
+    HEADER_FIELD_ERROR_NAME = 4
+    HEADER_FIELD_REPLY_SERIAL = 5
+    HEADER_FIELD_DESTINATION = 6
+    HEADER_FIELD_SENDER = 7
+    HEADER_FIELD_SIGNATURE = 8
+    HEADER_FIELD_UNIX_FDS = 9
+
+    HEADER_FIELD_LAST = HEADER_FIELD_UNIX_FDS
+
+    HEADER_SIGNATURE = bytes \
+      ((
+        TYPE_BYTE,
+        TYPE_BYTE,
+        TYPE_BYTE,
+        TYPE_BYTE,
+        TYPE_UINT32,
+        TYPE_UINT32,
+        TYPE_ARRAY,
+        STRUCT_BEGIN_CHAR,
+        TYPE_BYTE,
+        TYPE_VARIANT,
+        STRUCT_END_CHAR,
+      ))
+    MINIMUM_HEADER_SIZE = 16 # smallest header size that can occur (missing required fields, though)
+
+    # Errors
+    ERROR_FAILED = "org.freedesktop.DBus.Error.Failed" # generic error
+    ERROR_NO_MEMORY = "org.freedesktop.DBus.Error.NoMemory"
+    ERROR_SERVICE_UNKNOWN = "org.freedesktop.DBus.Error.ServiceUnknown"
+    ERROR_NAME_HAS_NO_OWNER = "org.freedesktop.DBus.Error.NameHasNoOwner"
+    ERROR_NO_REPLY = "org.freedesktop.DBus.Error.NoReply"
+    ERROR_IO_ERROR = "org.freedesktop.DBus.Error.IOError"
+    ERROR_BAD_ADDRESS = "org.freedesktop.DBus.Error.BadAddress"
+    ERROR_NOT_SUPPORTED = "org.freedesktop.DBus.Error.NotSupported"
+    ERROR_LIMITS_EXCEEDED = "org.freedesktop.DBus.Error.LimitsExceeded"
+    ERROR_ACCESS_DENIED = "org.freedesktop.DBus.Error.AccessDenied"
+    ERROR_AUTH_FAILED = "org.freedesktop.DBus.Error.AuthFailed"
+    ERROR_NO_SERVER = "org.freedesktop.DBus.Error.NoServer"
+    ERROR_TIMEOUT = "org.freedesktop.DBus.Error.Timeout"
+    ERROR_NO_NETWORK = "org.freedesktop.DBus.Error.NoNetwork"
+    ERROR_ADDRESS_IN_USE = "org.freedesktop.DBus.Error.AddressInUse"
+    ERROR_DISCONNECTED = "org.freedesktop.DBus.Error.Disconnected"
+    ERROR_INVALID_ARGS = "org.freedesktop.DBus.Error.InvalidArgs"
+    ERROR_FILE_NOT_FOUND = "org.freedesktop.DBus.Error.FileNotFound"
+    ERROR_FILE_EXISTS = "org.freedesktop.DBus.Error.FileExists"
+    ERROR_UNKNOWN_METHOD = "org.freedesktop.DBus.Error.UnknownMethod"
+    ERROR_UNKNOWN_OBJECT = "org.freedesktop.DBus.Error.UnknownObject"
+    ERROR_UNKNOWN_INTERFACE = "org.freedesktop.DBus.Error.UnknownInterface"
+    ERROR_UNKNOWN_PROPERTY = "org.freedesktop.DBus.Error.UnknownProperty"
+    ERROR_PROPERTY_READ_ONLY = "org.freedesktop.DBus.Error.PropertyReadOnly"
+    ERROR_TIMED_OUT = "org.freedesktop.DBus.Error.TimedOut"
+    ERROR_MATCH_RULE_NOT_FOUND = "org.freedesktop.DBus.Error.MatchRuleNotFound"
+    ERROR_MATCH_RULE_INVALID = "org.freedesktop.DBus.Error.MatchRuleInvalid"
+    ERROR_SPAWN_EXEC_FAILED = "org.freedesktop.DBus.Error.Spawn.ExecFailed"
+    ERROR_SPAWN_FORK_FAILED = "org.freedesktop.DBus.Error.Spawn.ForkFailed"
+    ERROR_SPAWN_CHILD_EXITED = "org.freedesktop.DBus.Error.Spawn.ChildExited"
+    ERROR_SPAWN_CHILD_SIGNALED = "org.freedesktop.DBus.Error.Spawn.ChildSignaled"
+    ERROR_SPAWN_FAILED = "org.freedesktop.DBus.Error.Spawn.Failed"
+    ERROR_SPAWN_SETUP_FAILED = "org.freedesktop.DBus.Error.Spawn.FailedToSetup"
+    ERROR_SPAWN_CONFIG_INVALID = "org.freedesktop.DBus.Error.Spawn.ConfigInvalid"
+    ERROR_SPAWN_SERVICE_INVALID = "org.freedesktop.DBus.Error.Spawn.ServiceNotValid"
+    ERROR_SPAWN_SERVICE_NOT_FOUND = "org.freedesktop.DBus.Error.Spawn.ServiceNotFound"
+    ERROR_SPAWN_PERMISSIONS_INVALID = "org.freedesktop.DBus.Error.Spawn.PermissionsInvalid"
+    ERROR_SPAWN_FILE_INVALID = "org.freedesktop.DBus.Error.Spawn.FileInvalid"
+    ERROR_SPAWN_NO_MEMORY = "org.freedesktop.DBus.Error.Spawn.NoMemory"
+    ERROR_UNIX_PROCESS_ID_UNKNOWN = "org.freedesktop.DBus.Error.UnixProcessIdUnknown"
+    ERROR_INVALID_SIGNATURE = "org.freedesktop.DBus.Error.InvalidSignature"
+    ERROR_INVALID_FILE_CONTENT = "org.freedesktop.DBus.Error.InvalidFileContent"
+    ERROR_SELINUX_SECURITY_CONTEXT_UNKNOWN = "org.freedesktop.DBus.Error.SELinuxSecurityContextUnknown"
+    ERROR_ADT_AUDIT_DATA_UNKNOWN = "org.freedesktop.DBus.Error.AdtAuditDataUnknown"
+    ERROR_OBJECT_PATH_IN_USE = "org.freedesktop.DBus.Error.ObjectPathInUse"
+    ERROR_INCONSISTENT_MESSAGE = "org.freedesktop.DBus.Error.InconsistentMessage"
+    ERROR_INTERACTIVE_AUTHORIZATION_REQUIRED = "org.freedesktop.DBus.Error.InteractiveAuthorizationRequired"
+
+    # XML introspection format
+    INTROSPECT_1_0_XML_NAMESPACE = "http://www.freedesktop.org/standards/dbus"
+    INTROSPECT_1_0_XML_PUBLIC_IDENTIFIER = "-//freedesktop//DTD D-BUS Object Introspection 1.0//EN"
+    INTROSPECT_1_0_XML_SYSTEM_IDENTIFIER = "http://www.freedesktop.org/standards/dbus/1.0/introspect.dtd"
+    INTROSPECT_1_0_XML_DOCTYPE_DECL_NODE = \
+        (
+            "<!DOCTYPE node PUBLIC \""
+        +
+            INTROSPECT_1_0_XML_PUBLIC_IDENTIFIER
+        +
+            "\"\n\"" + INTROSPECT_1_0_XML_SYSTEM_IDENTIFIER
+        +
+            "\">\n"
+        )
+
+    # from dbus-shared.h:
+
+    # well-known bus types
+    BusType = ct.c_uint
+    BUS_SESSION = 0
+    BUS_SYSTEM = 1
+    BUS_STARTER = 2
+
+    # results that a message handler can return
+    BusHandlerResult = ct.c_uint
+    HANDLER_RESULT_HANDLED = 0 # no need to try more handlers
+    HANDLER_RESULT_NOT_YET_HANDLED = 1 # see if other handlers want it
+    HANDLER_RESULT_NEED_MEMORY = 2 # try again later with more memory
+
+    # Bus names
+    SERVICE_DBUS = "org.freedesktop.DBus" # used to talk to the bus itself
+
+    # Paths
+    PATH_DBUS = "/org/freedesktop/DBus" # object path used to talk to the bus itself
+    PATH_LOCAL = "/org/freedesktop/DBus/Local" # path used in local/in-process-generated messages
+
+    # Interfaces
+    INTERFACE_DBUS = "org.freedesktop.DBus" # interface exported by the object with SERVICE_DBUS and PATH_DBUS
+    INTERFACE_MONITORING = "org.freedesktop.DBus.Monitoring" # monitoring interface exported by the dbus-daemon
+    INTERFACE_VERBOSE = "org.freedesktop.DBus.Verbose" # verbose interface exported by the dbus-daemon
+    INTERFACE_INTROSPECTABLE = "org.freedesktop.DBus.Introspectable" # interface supported by introspectable objects
+    INTERFACE_PROPERTIES = "org.freedesktop.DBus.Properties" # interface supported by objects with properties
+    INTERFACE_PEER = "org.freedesktop.DBus.Peer" # interface supported by most dbus peers
+    INTERFACE_LOCAL = "org.freedesktop.DBus.Local" # methods can only be invoked locally
+
+    # Owner flags for request_name
+    NAME_FLAG_ALLOW_REPLACEMENT = 0x1
+    NAME_FLAG_REPLACE_EXISTING = 0x2
+    NAME_FLAG_DO_NOT_QUEUE = 0x4
+
+    # Replies to request for a name
+    REQUEST_NAME_REPLY_PRIMARY_OWNER = 1
+    REQUEST_NAME_REPLY_IN_QUEUE = 2
+    REQUEST_NAME_REPLY_EXISTS = 3
+    REQUEST_NAME_REPLY_ALREADY_OWNER = 4
+
+    # Replies to releasing a name
+    RELEASE_NAME_REPLY_RELEASED = 1
+    RELEASE_NAME_REPLY_NON_EXISTENT = 2
+    RELEASE_NAME_REPLY_NOT_OWNER = 3
+
+    # Replies to service starts
+    START_REPLY_SUCCESS = 1
+    START_REPLY_ALREADY_RUNNING = 2
+
+    # from dbus-types.h etc:
 
     bool_t = ct.c_uint
 
@@ -66,9 +291,11 @@ class DBUS :
 #-
 
 dbus.dbus_connection_open.restype = ct.c_void_p
-dbus.dbus_connection_open.argtypes = (ct.c_char_p, ct.c_void_p)
+dbus.dbus_connection_open.argtypes = (ct.c_char_p, DBUS.ErrorPtr)
 dbus.dbus_connection_open_private.restype = ct.c_void_p
-dbus.dbus_connection_open_private.argtypes = (ct.c_char_p, ct.c_void_p)
+dbus.dbus_connection_open_private.argtypes = (ct.c_char_p, DBUS.ErrorPtr)
+dbus.dbus_connection_close.restype = None
+dbus.dbus_connection_close.argtypes = (ct.c_void_p,)
 dbus.dbus_connection_read_write.restype = DBUS.bool_t
 dbus.dbus_connection_read_write.argtypes = (ct.c_void_p, ct.c_int)
 dbus.dbus_connection_read_write_dispatch.restype = DBUS.bool_t
@@ -90,6 +317,36 @@ dbus.dbus_connection_unref.argtypes = (ct.c_void_p,)
 dbus.dbus_connection_unregister_object_path.restype = DBUS.bool_t
 dbus.dbus_connection_unregister_object_path.argtypes = (ct.c_void_p, ct.c_char_p)
 
+dbus.dbus_bus_get.restype = ct.c_void_p
+dbus.dbus_bus_get.argtypes = (ct.c_uint, DBUS.ErrorPtr)
+dbus.dbus_bus_get_private.restype = ct.c_void_p
+dbus.dbus_bus_get_private.argtypes = (ct.c_uint, DBUS.ErrorPtr)
+dbus.dbus_bus_register.restype = DBUS.bool_t
+dbus.dbus_bus_register.argtypes = (ct.c_void_p, DBUS.ErrorPtr)
+dbus.dbus_bus_set_unique_name.restype = DBUS.bool_t
+dbus.dbus_bus_set_unique_name.argtypes = (ct.c_void_p, ct.c_char_p)
+dbus.dbus_bus_get_unique_name.restype = ct.c_char_p
+dbus.dbus_bus_get_unique_name.argtypes = (ct.c_void_p,)
+dbus.dbus_bus_get_unix_user.restype = ct.c_ulong
+dbus.dbus_bus_get_unix_user.argtypes = (ct.c_void_p, ct.c_char_p, DBUS.ErrorPtr)
+dbus.dbus_bus_get_id.restype = ct.c_char_p
+dbus.dbus_bus_get_id.argtypes = (ct.c_void_p, DBUS.ErrorPtr)
+dbus.dbus_bus_request_name.restype = ct.c_int
+dbus.dbus_bus_request_name.argtypes = (ct.c_void_p, ct.c_char_p, ct.c_uint, DBUS.ErrorPtr)
+dbus.dbus_bus_release_name.restype = ct.c_int
+dbus.dbus_bus_release_name.argtypes = (ct.c_void_p, ct.c_char_p, DBUS.ErrorPtr)
+dbus.dbus_bus_name_has_owner.restype = DBUS.bool_t
+dbus.dbus_bus_name_has_owner.argtypes = (ct.c_void_p, ct.c_char_p, DBUS.ErrorPtr)
+dbus.dbus_bus_start_service_by_name.restype = DBUS.bool_t
+dbus.dbus_bus_start_service_by_name.argtypes = (ct.c_void_p, ct.c_char_p, ct.c_uint, ct.POINTER(ct.c_uint), DBUS.ErrorPtr)
+dbus.dbus_bus_add_match.restype = None
+dbus.dbus_bus_add_match.argtypes = (ct.c_void_p, ct.c_char_p, DBUS.ErrorPtr)
+dbus.dbus_bus_remove_match.restype = None
+dbus.dbus_bus_remove_match.argtypes = (ct.c_void_p, ct.c_char_p, DBUS.ErrorPtr)
+
+dbus.dbus_message_get_serial.restype = ct.c_uint
+dbus.dbus_message_get_serial.argtypes = (ct.c_void_p,)
+
 dbus.dbus_error_init.restype = None
 dbus.dbus_error_init.argtypes = (DBUS.ErrorPtr,)
 dbus.dbus_error_free.restype = None
@@ -108,10 +365,175 @@ dbus.dbus_set_error.argtypes = (DBUS.ErrorPtr, ct.c_char_p, ct.c_char_p, ct.c_ch
 # High-level stuff follows
 #-
 
+class ObjectPathVTable :
+
+    __slots__ = \
+      (
+        "_dbobj",
+        # need to keep references to ctypes-wrapped functions
+        # so they don't disappear prematurely:
+        "_wrap_unregister_func",
+        "_wrap_message_func",
+      ) # to forestall typos
+
+    def __init__(self, *, unregister = None, message = None) :
+        self._dbobj = DBUS.ObjectPathVTable()
+        self._wrap_unregister_func = None
+        self._wrap_message_func = None
+        if unregister != None :
+            self.set_unregister(unregister)
+        #end if
+        if message != None :
+            self.set_message(message)
+        #end if
+    #end __init__
+
+    def set_unregister(self, unregister = None) :
+
+        def wrap_unregister(c_conn, user_data) :
+            unregister(Connection(dbus.dbus_connection_ref(c_conn)), user_data)
+        #end wrap_unregister
+
+    #begin set_unregister
+        if unregister != None :
+            self._wrap_unregister_func = DBUS.ObjectPathUnregisterFunction(wrap_unregister)
+        else :
+            self._wrap_unregister_func = None
+        #end if
+        self._dbobj.unregister_function = self._wrap_unregister_func
+        return \
+            self
+    #end set_unregister
+
+    def set_message(self, message = None) :
+
+        def wrap_message(c_conn, c_message, user_data) :
+            return \
+                message(Connection(dbus.dbus_connection_ref(c_conn)), Message(c_message), user_data)
+        #end wrap_message
+
+    #begin set_message
+        if message != None :
+            self._wrap_message_func = DBUS.ObjectPathMessageFunction(wrap_message)
+        else :
+            self._wrap_message_func = None
+        #end if
+        self._dbobj.message = self._wrap_message_func
+        return \
+            self
+    #end set_message
+
+#end ObjectPathVTable
+
+class _DummyError :
+
+    def raise_if_set(self) :
+        pass
+    #end raise_if_set
+
+#end _DummyError
+
+def _get_error(error) :
+    if error != None and not isinstance(error, Error) :
+        raise TypeError("error must be an Error")
+    #end if
+    if error != None :
+        my_error = _DummyError()
+    else :
+        my_error = Error()
+        error = my_error
+    #end if
+    return \
+        error, my_error
+#end _get_error
+
 class Connection :
     "wrapper around a DBusConnection object."
     # <https://dbus.freedesktop.org/doc/api/html/group__DBusConnection.html>
-    pass # TBD
+
+    __slots__ = ("__weakref__", "_dbobj",) # to forestall typos
+
+    _instances = WeakValueDictionary()
+
+    def __new__(celf, _dbobj) :
+        self = celf._instances.get(_dbobj)
+        if self == None :
+            self = super().__new__(celf)
+            self._dbobj = _dbobj
+            celf._instances[_dbobj] = self
+        else :
+            dbus.dbus_connection_unref(self._dbobj)
+              # lose extra reference created by caller
+        #end if
+        return \
+            self
+    #end __new__
+
+    def __del__(self) :
+        if self._dbobj != None :
+            dbus.dbus_connection_unref(self._dbobj)
+            self._dbobj = None
+        #end if
+    #end __del__
+
+    @classmethod
+    def open(celf, address, private, error = None) :
+        error, my_error = _get_error(error)
+        result = (dbus.dbus_connection_open, dbus.dbus_connection_open_private)[private](address, error._dbobj)
+        my_error.raise_if_set()
+        if result != None :
+            result = celf(result)
+        #end if
+        return \
+            result
+    #end open
+
+    def close(self) :
+        dbus.dbus_connection_close(self._dbobj)
+    #end close
+
+    # message bus APIs
+    # <https://dbus.freedesktop.org/doc/api/html/group__DBusBus.html>
+
+    @classmethod
+    def bus_get(celf, type, private, error = None) :
+        "type is a BUS_xxx value."
+        error, my_error = _get_error(error)
+        result = (dbus.dbus_bus_get, dbus.dbus_bus_get_private)[private](type, error._dbobj)
+        my_error.raise_if_set()
+        if result != None :
+            result = celf(result)
+        #end if
+        return \
+            result
+    #end bus_get
+
+    def bus_register(self, error = None) :
+        error, my_error = _get_error(error)
+        dbus.dbus_bus_register(self._dbobj, error._dbobj)
+        my_error.raise_if_set()
+    #end bus_register
+
+    def bus_request_name(self, name, flags, error = None) :
+        "flags is a combination of NAME_FLAG_xxx bits. Result will be" \
+        " a REQUEST_NAME_REPLY_xxx value or -1 on error."
+        error, my_error = _get_error(error)
+        result = dbus.dbus_bus_request_name(self._dbobj, name.encode(), flags, error._dbobj)
+        my_error.raise_if_set()
+        return \
+            result
+    #end bus_request_name
+
+    def bus_release_name(self, name, error = None) :
+        error, my_error = _get_error(error)
+        result = dbus.dbus_bus_release_name(self._dbobj, name.encode(), error._dbobj)
+        my_error.raise_if_set()
+        return \
+            result
+    #end bus_release_name
+
+    # more TBD
+
 #end Connection
 
 class Message :
@@ -165,6 +587,12 @@ class Error :
         return \
             self._dbobj.message.decode()
     #end message
+
+    def raise_if_set(self) :
+        if self.is_set :
+            raise RuntimeError("D-Bus error %s: %s" % (self.name, self.message))
+        #end if
+    #end raise_if_set
 
 #end Error
 

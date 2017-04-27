56f1a366c8a482c63782933514222376bb827a8a
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2017-04-27 03:08:05 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#+
+# Distutils script to install DBussy. Invoke from the command line
+# in this directory as follows:
+#
+#     python3 setup.py build
+#     sudo python3 setup.py install
+#
+# Written by Lawrence D'Oliveiro <ldo@geek-central.gen.nz>.
+#-
+
+import distutils.core
+
+distutils.core.setup \
+  (
+    name = "DBussy",
+    version = "0.5",
+    description = "language bindings for libdbus, for Python 3.5 or later",
+    author = "Lawrence D'Oliveiro",
+    author_email = "ldo@geek-central.gen.nz",
+    url = "http://github.com/ldo/dbussy",
+    py_modules = ["dbussy"],
+  )

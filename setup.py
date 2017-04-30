eeb00d12a93d87792af6f8cc4866ef08aa715e5b
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2017-04-30 16:47:31 +0000

    add Python version check

--- setup.py
+++ setup.py
@@ -8,7 +8,30 @@
 # Written by Lawrence D'Oliveiro <ldo@geek-central.gen.nz>.
 #-
 
+import sys
 import distutils.core
+from distutils.command.build import \
+    build as std_build
+
+class my_build(std_build) :
+    "customization of build to perform additional validation."
+
+    def run(self) :
+        try :
+            exec \
+              (
+                "async def dummy() :\n"
+                "    pass\n"
+                "#end dummy\n"
+              )
+        except SyntaxError :
+            sys.stderr.write("This module requires Python 3.5 or later.\n")
+            sys.exit(-1)
+        #end try
+        super().run()
+    #end run
+
+#end my_build
 
 distutils.core.setup \
   (
@@ -19,4 +42,8 @@ distutils.core.setup \
     author_email = "ldo@geek-central.gen.nz",
     url = "http://github.com/ldo/dbussy",
     py_modules = ["dbussy", "ravel"],
+    cmdclass =
+        {
+            "build" : my_build,
+        },
   )

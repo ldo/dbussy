cd6c367b9b76a9a91a17227f0da7b25219e400f5
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2019-09-16 07:18:50 +0000

    change URL to HTTPS

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ distutils.core.setup \
     long_description = "language bindings for libdbus, for Python 3.5 or later",
     author = "Lawrence D'Oliveiro",
     author_email = "ldo@geek-central.gen.nz",
-    url = "http://github.com/ldo/dbussy",
+    url = "https://github.com/ldo/dbussy",
     license = "LGPL v2.1+",
     py_modules = ["dbussy", "ravel"],
     cmdclass =

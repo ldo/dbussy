d0ec0223f3797e1612d835e71694a1083881149f
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2019-09-23 03:16:47 +0000

    bump version

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class my_build(std_build) :
 distutils.core.setup \
   (
     name = "DBussy",
-    version = "1.2",
+    version = "1.2.1",
     description = "language bindings for libdbus, for Python 3.5 or later",
     long_description = "language bindings for libdbus, for Python 3.5 or later",
     author = "Lawrence D'Oliveiro",
